97109b54af0f74bdb6256f968335b771df4b07c4
湯田佳菜 <kanasama@wlan-napt-001.u-aizu.ac.jp>
2021-07-08 22:11:02 +0900

    Greetings added

---
+++ Die_2.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Greeting{
+  void askName(){
+     System.out.println("What is your name?");
+   }
+  String scanName(){
+    Scanner sc1=new Scanner(System.in);
+    return sc1.nextLine();
+  }
+  void printName(){
+    String name=scanName();
+    System.out.println("Hello,"+name+"!");
+}
+}
+
+class Create{
+  int Rand;
+  Create(){
+    Rand=0;
+  }
+  int Rand_create(){
+    Random rand=new Random();
+    Rand=rand.nextInt(6)+1;
+    return Rand;
+  }
+}
+
+class Die_2{
+  public static void main(String[] args){
+    int d1;
+    int d2;
+    int total;
+
+    Greeting g=new Greeting();
+    g.askName();
+    g.printName();
+
+    Create c1=new Create();
+    d1=c1.Rand_create();
+
+    Create c2=new Create();
+    d2=c2.Rand_create();
+
+    total=d1+d2;
+
+    System.out.println("Rolling the dice...");
+    System.out.println("Die 1: "+d1);
+    System.out.println("Die 2: "+d2);
+    System.out.println("Total value: "+total);
+  }
+}
